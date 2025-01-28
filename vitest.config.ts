669ee620fe3d6ab34aad1e7782fba07e9871c74d
Manuel Zedel <manuel.zedel@northern.tech>
2025-01-28 10:14:54 +0100

    test: made vitest rely on dependency packages instead of aliases
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,8 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: '@northern.tech/store',
-        replacement: path.resolve(__dirname, 'packages/store/src')
-      },
-      {
-        find: '@northern.tech/common-ui',
-        replacement: path.resolve(__dirname, 'packages/common-ui/src/')
-      },
-      {
-        find: '@northern.tech/helptips',
-        replacement: path.resolve(__dirname, 'packages/common-ui/src/helptips')
-      },
-      {
-        find: '@northern.tech/utils',
-        replacement: path.resolve(__dirname, 'packages/utils/src')
-      }
-    ]
-  },
   server: {
     port: 80,
     middlewareMode: false
