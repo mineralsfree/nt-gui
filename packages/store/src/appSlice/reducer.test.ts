5aac055421ef4cb077f6efb8e14c7f6adc66a8cd
Mikita Pilinka <mikita.pilinka@northern.tech>
2025-03-18 18:58:09 +0100

    test: adjusted tests to use typescript
    Ticket: MEN-8206
    Changelog: None
    
    Signed-off-by: Mikita Pilinka <mikita.pilinka@northern.tech>

--- packages/store/src/appSlice/reducer.test.ts
+++ packages/store/src/appSlice/reducer.test.ts
@@ -11,8 +11,8 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-// @ts-nocheck
 import { SORTING_OPTIONS } from '@northern.tech/store/commonConstants';
+import { describe, expect, it } from 'vitest';
 
 import reducer, { actions, initialState } from '.';
 
@@ -21,77 +21,78 @@ const initialSearchState = {
   deviceIds: [],
   searchTerm: '',
   searchTotal: 0,
+  isSearching: false,
   sort: { direction: SORTING_OPTIONS.desc }
 };
 
 describe('app reducer', () => {
   it('should return the initial state', async () => {
-    expect(reducer(undefined, {})).toEqual(initialState);
+    expect(reducer(undefined, { type: '' })).toEqual(initialState);
   });
   it('should handle SET_SNACKBAR', async () => {
-    expect(reducer(undefined, { type: actions.setSnackbar, payload: { open: true, message: snackbarMessage } }).snackbar).toEqual({
+    expect(reducer(undefined, { type: actions.setSnackbar.type, payload: { open: true, message: snackbarMessage } }).snackbar).toEqual({
+      action: undefined,
+      autoHideDuration: undefined,
+      message: snackbarMessage,
       open: true,
-      maxWidth: 900,
-      message: snackbarMessage
+      preventClickToCopy: false
     });
 
-    expect(reducer(initialState, { type: actions.setSnackbar, payload: { open: true, message: snackbarMessage } }).snackbar).toEqual({
+    expect(reducer(initialState, { type: actions.setSnackbar.type, payload: { open: true, message: snackbarMessage } }).snackbar).toEqual({
+      action: undefined,
+      autoHideDuration: undefined,
+      message: snackbarMessage,
       open: true,
-      maxWidth: 900,
-      message: snackbarMessage
+      preventClickToCopy: false
     });
   });
 
   it('should handle SET_FIRST_LOGIN_AFTER_SIGNUP', async () => {
-    expect(reducer(undefined, { type: actions.setFirstLoginAfterSignup, payload: true }).firstLoginAfterSignup).toEqual(true);
+    expect(reducer(undefined, { type: actions.setFirstLoginAfterSignup.type, payload: true }).firstLoginAfterSignup).toEqual(true);
 
-    expect(reducer(initialState, { type: actions.setFirstLoginAfterSignup, payload: false }).firstLoginAfterSignup).toEqual(false);
+    expect(reducer(initialState, { type: actions.setFirstLoginAfterSignup.type, payload: false }).firstLoginAfterSignup).toEqual(false);
   });
   it('should handle SET_ANNOUNCEMENT', async () => {
-    expect(reducer(undefined, { type: actions.setAnnouncement, payload: 'something' }).hostedAnnouncement).toEqual('something');
-    expect(reducer(initialState, { type: actions.setAnnouncement, payload: undefined }).hostedAnnouncement).toEqual(undefined);
+    expect(reducer(undefined, { type: actions.setAnnouncement.type, payload: 'something' }).hostedAnnouncement).toEqual('something');
+    expect(reducer(initialState, { type: actions.setAnnouncement.type, payload: undefined }).hostedAnnouncement).toEqual(undefined);
   });
   it('should handle SET_SEARCH_STATE', async () => {
-    expect(reducer(undefined, { type: actions.setSearchState, payload: { aWhole: 'newState' } }).searchState).toEqual({
+    expect(reducer(undefined, { type: actions.setSearchState.type, payload: { aWhole: 'newState' } }).searchState).toEqual({
       ...initialSearchState,
       aWhole: 'newState'
     });
-    expect(reducer(initialState, { type: actions.setSearchState, payload: undefined }).searchState).toEqual({ ...initialSearchState });
+    expect(reducer(initialState, { type: actions.setSearchState.type, payload: undefined }).searchState).toEqual({ ...initialSearchState });
   });
   it('should handle SET_OFFLINE_THRESHOLD', async () => {
-    expect(reducer(undefined, { type: actions.setOfflineThreshold, payload: 'something' }).offlineThreshold).toEqual('something');
-    expect(reducer(initialState, { type: actions.setOfflineThreshold, payload: undefined }).offlineThreshold).toEqual(undefined);
+    expect(reducer(undefined, { type: actions.setOfflineThreshold.type, payload: 'something' }).offlineThreshold).toEqual('something');
+    expect(reducer(initialState, { type: actions.setOfflineThreshold.type, payload: undefined }).offlineThreshold).toEqual(undefined);
   });
 
   const versionInformation = {
-    Deployments: '',
-    Deviceauth: '',
-    GUI: 'latest',
     Integration: '',
-    Inventory: '',
     'Mender-Artifact': '',
     'Mender-Client': '',
     'Meta-Mender': ''
   };
   it('should handle SET_VERSION_INFORMATION', async () => {
-    expect(reducer(undefined, { type: actions.setVersionInformation, payload: { something: 'something' } }).versionInformation).toEqual({
+    expect(reducer(undefined, { type: actions.setVersionInformation.type, payload: { something: 'something' } }).versionInformation).toEqual({
       ...versionInformation,
       something: 'something'
     });
-    expect(reducer(initialState, { type: actions.setVersionInformation, payload: undefined }).versionInformation).toEqual(versionInformation);
-    expect(reducer(undefined, { type: actions.setVersionInformation, payload: { docsVersion: 'something' } }).versionInformation.docsVersion).toEqual(
+    expect(reducer(initialState, { type: actions.setVersionInformation.type, payload: undefined }).versionInformation).toEqual(versionInformation);
+    expect(reducer(undefined, { type: actions.setVersionInformation.type, payload: { docsVersion: 'something' } }).versionInformation.docsVersion).toEqual(
       'something'
     );
-    expect(reducer(initialState, { type: actions.setVersionInformation, payload: { docsVersion: undefined } }).versionInformation.docsVersion).toEqual(
+    expect(reducer(initialState, { type: actions.setVersionInformation.type, payload: { docsVersion: undefined } }).versionInformation.docsVersion).toEqual(
       undefined
     );
   });
 
   it('should handle UPLOAD_PROGRESS', async () => {
-    const { uploadsById } = reducer(undefined, { type: actions.uploadProgress, payload: { id: 1, progress: 40 } });
+    const { uploadsById } = reducer(undefined, { type: actions.uploadProgress.type, payload: { id: 1, progress: 40 } });
     expect(uploadsById['1']).toEqual({ progress: 40 });
 
-    const { uploadsById: uploading2 } = reducer(initialState, { type: actions.uploadProgress, payload: { id: 'foo', progress: 40 } });
+    const { uploadsById: uploading2 } = reducer(initialState, { type: actions.uploadProgress.type, payload: { id: 'foo', progress: 40 } });
     expect(uploading2.foo).toEqual({ progress: 40 });
   });
 });
--- packages/store/src/appSlice/thunks.test.ts
+++ packages/store/src/appSlice/thunks.test.ts
@@ -11,11 +11,11 @@
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
-// @ts-nocheck
 import { commonErrorHandler } from '@northern.tech/store/store';
 import { searchDevices } from '@northern.tech/store/thunks';
 import configureMockStore from 'redux-mock-store';
 import { thunk } from 'redux-thunk';
+import { describe, expect, it } from 'vitest';
 
 import { actions } from '.';
 import { defaultState } from '../../../../tests/mockData';
